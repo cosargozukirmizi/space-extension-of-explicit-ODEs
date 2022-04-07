60580a72f7c7c51794028aa1393b2eb1a4232f41
cosargozukirmizi <cosargozukirmizi@users.noreply.github.com>
2022-04-07 16:47:08 +0300

    generalize to n dimensions

--- main.cpp
+++ main.cpp
@@ -1,44 +1,91 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
-void extendSpace (const vector<int> equationVector);
+void extendSpace (const vector<int> equationVector, const int stEq);
 
 int main ()
 {
   const vector<int> vanderPol
   {
-  1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 2, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0};
+    1, 0,  0, 0,  1, 0,
+    1, 0,  1, 0,  2, 0,
+    1, 0,  0, 0,  0, 1,
+    0, 1,  0, 0,  1, 0
+  };
 
-  cout << "\nThe space extension for van der Pol ODE is";
+  cout << "\n The space extension for van der Pol ODE is";
   cout << endl;
-  extendSpace (vanderPol);
+  extendSpace (vanderPol, 2);
   cout << "--------------------------------\n\n";
 
   const vector<int> quarticAnharmonicOscillator
   {
-  1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 2, 0};
+    1, 0,  0, 0,  0, 1,
+    0, 1,  0, 0,  1, 0,
+    0, 1,  1, 0,  2, 0
+  };
 
-  cout << "\nThe space extension for classical quartic anharmonic oscillator ODE is";
+  cout << "\n The space extension for classical quartic anharmonic oscillator ODE is";
   cout << endl;
-  extendSpace (quarticAnharmonicOscillator);
+  extendSpace (quarticAnharmonicOscillator, 2);
   cout << "--------------------------------\n\n";
 
   const vector<int> highPowers
   {
-  1, 0, 50, 50, 50, 50, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 2, 0};
+    1, 0,  50, 50,  50, 50,
+    0, 1,   0,  0,   1,  0,
+    0, 1,   1,  0,   2,  0
+  };
 
-  cout << "\nThe space extension for ODE with high powers is";
+  cout << "\n The space extension for ODE with high powers is";
   cout << endl;
-  extendSpace (highPowers);
+  extendSpace (highPowers, 2);
+  cout << "--------------------------------\n\n";
+
+  const vector<int> henonHeiles
+  {
+    1, 0, 0, 0,  0, 0, 0, 0,  0, 1, 0, 0,
+    0, 1, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0,
+    0, 1, 0, 0,  0, 0, 1, 0,  1, 0, 0, 0,
+    0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 1,
+    0, 0, 0, 1,  0, 0, 0, 0,  0, 0, 1, 0,
+    0, 0, 0, 1,  1, 0, 0, 0,  1, 0, 0, 0,
+    0, 0, 0, 1,  0, 0, 1, 0,  0, 0, 1, 0
+  };
+
+  cout << "\n The space extension for Henon-Heiles ODE is";
+  cout << endl;
+  extendSpace (henonHeiles, 4);
+  cout << "--------------------------------\n\n";
+
+  const vector<int> rabinovichFabrikant
+  {
+    1, 0, 0,  0, 0, 1,  0, 1, 0,
+    1, 0, 0,  0, 0, 0,  0, 1, 0,
+    1, 0, 0,  1, 0, 0,  1, 1, 0,
+    1, 0, 0,  0, 0, 0,  1, 0, 0,
+    0, 1, 0,  0, 0, 1,  1, 0, 0,
+    0, 1, 0,  0, 0, 0,  1, 0, 0,
+    0, 1, 0,  1, 0, 0,  2, 0, 0,
+    0, 1, 0,  0, 0, 0,  0, 1, 0,
+    0, 0, 1,  0, 0, 0,  0, 0, 1,
+    0, 0, 1,  0, 1, 0,  1, 0, 1
+  };
+
+  cout << "\n The space extension for Rabinovich-Fabrikant ODE is";
+  cout << endl;
+  extendSpace (rabinovichFabrikant, 3);
   cout << "--------------------------------\n\n";
 
   return 0;
 }
 
 
-void extendSpace (const vector<int> equationVector)
+void extendSpace (vector<int> equationVector, const int stEq)
 {
 
   vector<int> runVec = equationVector;
@@ -52,84 +99,98 @@ void extendSpace (const vector<int> equationVector)
   int temp4 = 0;
   int temp5 = 0;
   int temp6 = 0;
-  int numEqs = 2;  // start with two unknowns
+  int numEqs = stEq;
 
-  vector<int> rightHandSide{0, 0};
+  vector<int> rightHandSide (stEq, 0);
 
   while (i < runVec.size ())
   {
-    for (int j = 2; j <= 4; j += 2)
+    for (int j = stEq; j <= 2 * stEq; j += stEq)
     {
-      rightHandSide[0] = runVec[i + j];
-      rightHandSide[1] = runVec[i + j + 1];
-
-      for (int i = 0; i < runVec.size (); i += 6)
+      for (int k = 0; k < stEq; k++)
       {
-	if (rightHandSide[0] == runVec[i] && rightHandSide[1] == runVec[i + 1])
-	{
-	  appears = 1;
-	  break;
-	}
+        rightHandSide[k] = runVec[i + j + k];
+      }
+      for (int i = 0; i < runVec.size (); i += 3 * stEq)
+      {
+        vector<int> subvector(stEq, 0);
+        subvector = {runVec.begin () + i, runVec.begin () + i + stEq};
+        if (subvector == rightHandSide)
+        {
+          appears = 1;
+          break;
+        }
       }
 
       if (appears == 0)
       {
         numEqs++;
-	if (rightHandSide[0] == 0 && rightHandSide[1] == 0)
-	{
-	  runVec.insert (runVec.end (),
-	                 {0, 0, 0, 0, 0, 0});
-	}
-	else
-	{
+        vector<int> allZero(stEq,0);
+        vector<int> zeroEntry(3*stEq, 0);
 
-	  for (int i = 0; i < equationVector.size (); i += 6)
-	  {
-	    if (equationVector[i] == 1 && equationVector[i + 1] == 0 && rightHandSide[0] > 0)
-	    {
-		temp =
-		  equationVector[i + 2] + equationVector[i + 4] +
-		  rightHandSide[0] - 1;
-		temp2 =
-		  equationVector[i + 3] + equationVector[i + 5] +
-		  rightHandSide[1];
-            }
-	    else if (equationVector[i] == 0 && equationVector[i + 1] == 1 && rightHandSide[1] > 0)
+        if (rightHandSide == allZero)
+        {
+          runVec.insert (runVec.end (), zeroEntry.begin(), zeroEntry.end() );
+        }
+        else
+        {
+          for (int i = 0; i < equationVector.size (); i += 3*stEq)
+          {
+            std::vector<int>::iterator it;
+            it = std::find (equationVector.begin () + i, equationVector.begin () + i + 3*stEq, 1);
+            int oneInd = (it - equationVector.begin ());
+
+            bool x = 0;
+            vector<int> myTemp(stEq, 0);
+
+            for (int j = 0; j < stEq; j++)
             {
-		temp =
-		  equationVector[i + 2] + equationVector[i + 4] +
-		  rightHandSide[0];
-		temp2 =
-		  equationVector[i + 3] + equationVector[i + 5] +
-		  rightHandSide[1] - 1;
-            }
-            else
-            {
-                continue;
+              if (oneInd == i+j && rightHandSide[j] > 0)
+              {
+                x = 1;
+                for (int k = 0 ; k < stEq; k++)
+                {
+                  myTemp[k] =
+                    equationVector[i + stEq + k] + equationVector[i + stEq + stEq + k] +
+                    rightHandSide[k];
+                  if (j==k)
+                  {
+                    myTemp[k] -= 1;
+                  }
+                }
+              }
             }
 
-            if (temp == 1 && temp2 == 1)
+            if (x == 0)
             {
-		runVec.push_back (rightHandSide[0]);
-		runVec.push_back (rightHandSide[1]);
-		runVec.push_back (0);
-		runVec.push_back (1);
-		runVec.push_back (1);
-		runVec.push_back (0);
+              continue;
             }
-	    else
-	    {
-		temp3 = temp / 2;
-		temp5 = temp - temp3;
-		temp4 = temp2 / 2;
-		temp6 = temp2 - temp4;
-		runVec.push_back (rightHandSide[0]);
-		runVec.push_back (rightHandSide[1]);
-		runVec.push_back (temp3);
-		runVec.push_back (temp4);
-		runVec.push_back (temp5);
-		runVec.push_back (temp6);
-	    }
+            vector<int> lefts;
+            int oneCount = 0;
+
+            for (int counter = 0; counter < stEq; counter++)
+              runVec.push_back (rightHandSide[counter]);
+
+            for (int i = 0; i < myTemp.size (); i++)
+            {
+              if (myTemp[i] == 1)
+                oneCount++;
+
+              if (myTemp[i] != 1 || (myTemp[i]==1 && (oneCount%2==1)))
+              {
+                lefts.push_back(myTemp[i] / 2);
+                runVec.push_back(myTemp[i] / 2);
+              }
+              else if (myTemp[i]==1 && oneCount%2==0)
+              {
+                lefts.push_back(1);
+                runVec.push_back(1);
+              }
+            }
+            for (int i = 0; i < lefts.size (); i++)
+            {
+              runVec.push_back(myTemp[i] - lefts[i]);
+            }
           }
         }
       }
@@ -137,18 +198,22 @@ void extendSpace (const vector<int> equationVector)
       appears = 0;
     }
 
-    i += 6;
+    i += 3*stEq;
   }
 
   for (auto i = 0; i < runVec.size (); i++)
   {
-    cout << runVec[i] << ' ';
+    cout << setw(3);
+    cout << runVec[i];
 
-    if (i % 6 == 5)
+    if (i % stEq == stEq-1)
+      cout << ' ';
+
+    if (i % (3*stEq) == (3*stEq-1))
       cout << endl;
   }
 
-  cout << "There are " << numEqs << " equations.\n";
+  cout << " There are " << numEqs << " equations.\n";
 
   return;
 }
